aea2d35bd8c9ac0ed09daacba7f7eb4e47d4be33
Sraleik <nicolas.rotier@gmail.com>
2023-07-19 17:54:05 +0200

    feat(sorci-event): add tests for sorci-event and add a sorciEventFactory

---
+++ src/sorci-event.test.ts
@@ -0,0 +1,96 @@
+import { SorciEvent, sorciEventFactory } from "./sorci-event";
+import { ulid } from "ulidx";
+import { v4 as uuid } from "uuid";
+import { customAlphabet } from "nanoid";
+
+describe("Given the default SorciEvent", () => {
+  describe("When creating a new SorciEvent", () => {
+    const eventPayload = {
+      type: "course-created",
+      data: { name: "Maths", courseId: ulid() },
+    };
+
+    const event = SorciEvent.create(eventPayload);
+
+    test("Then the event id is an ulid", () => {
+      expect(event.id).toMatch(/^[\dA-HJKMNP-TV-Z]{26}$/);
+    });
+    test("Then the type is correct", () => {
+      expect(event.type).toEqual(eventPayload.type);
+    });
+    test("Then the data is correct", () => {
+      expect(event.data).toEqual(eventPayload.data);
+    });
+    test("Then the identifier is correct", () => {
+      expect(event.identifier).toEqual({
+        courseId: eventPayload.data.courseId,
+      });
+    });
+    test("Then the timestamp is correct", () => {
+      expect(event.timestamp).toBeInstanceOf(Date);
+    });
+  });
+});
+
+describe("Given SorciEvent customize to use uuid", () => {
+  describe("When creating a new SorciEvent", () => {
+    const eventPayload = {
+      type: "course-created",
+      data: { name: "Maths", courseId: uuid() },
+    };
+
+    const SorciEventUuid = sorciEventFactory(() =>  uuid())
+
+    const event = SorciEventUuid.create(eventPayload);
+
+    test("Then the event id is an ulid", () => {
+      expect(event.id).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[8|9aAbB][0-9a-f]{3}-[0-9a-f]{12}$/i);
+    });
+    test("Then the type is correct", () => {
+      expect(event.type).toEqual(eventPayload.type);
+    });
+    test("Then the data is correct", () => {
+      expect(event.data).toEqual(eventPayload.data);
+    });
+    test("Then the identifier is correct", () => {
+      expect(event.identifier).toEqual({
+        courseId: eventPayload.data.courseId,
+      });
+    });
+    test("Then the timestamp is correct", () => {
+      expect(event.timestamp).toBeInstanceOf(Date);
+    });
+  });
+});
+
+describe("Given SorciEvent customize to use nanoid", () => {
+  describe("When creating a new SorciEvent", () => {
+    const tinyId = customAlphabet('0123456789abcdef', 12)
+    const eventPayload = {
+      type: "course-created",
+      data: { name: "Maths", courseId: tinyId() },
+    };
+
+    const SorciEventUuid = sorciEventFactory(() =>  tinyId())
+
+    const event = SorciEventUuid.create(eventPayload);
+
+    test("Then the event id is an ulid", () => {
+      expect(event.id).toMatch(/^[\da-f]{12}$/i);
+    });
+    test("Then the type is correct", () => {
+      expect(event.type).toEqual(eventPayload.type);
+    });
+    test("Then the data is correct", () => {
+      expect(event.data).toEqual(eventPayload.data);
+    });
+    test("Then the identifier is correct", () => {
+      expect(event.identifier).toEqual({
+        courseId: eventPayload.data.courseId,
+      });
+    });
+    test("Then the timestamp is correct", () => {
+      expect(event.timestamp).toBeInstanceOf(Date);
+    });
+  });
+});
--- src/sorci-event.ts
+++ src/sorci-event.ts
@@ -1,4 +1,4 @@
-import { createId } from "./common/utils";
+import { createId as createIdDefault } from "./common/utils";
 import { EventId, PersistedEvent } from "./sorci.interface";
 
 export type SorciEventCreatePayload = {
@@ -16,19 +16,21 @@ export type SorciEventReconstitutePayload = Omit<
   timestamp: Date;
 };
 
+export type createIdType = (id?: string) => EventId;
+
 /**
  * This class is an helper to focus on the relevant part of your sourcing events
  * Wich are the `type` of event and the `data` of the event.
- * 
- * If you don't want to be heavily coupled with this library you could make your own 
- * implementation of it and use the type {@link ToPersistEvent} to append event and 
+ *
+ * If you don't want to be heavily coupled with this library you could make your own
+ * implementation of it and use the type {@link ToPersistEvent} to append event and
  * {@link PersistedEvent} to query events
  *
  * @remarks The class will genereate automatically the identifier if not provided
  */
 export class SorciEvent {
   /**
-   * This is used to automatically create the identifier. 
+   * This is used to automatically create the identifier.
    * It retrieve every key/pair from the data object where the 'key' end with 'Id'
    */
   static extractKeyValuePairContainingId(payload: Record<string, any>) {
@@ -43,7 +45,7 @@ export class SorciEvent {
     }, {});
   }
 
-  static create(payload: SorciEventCreatePayload) {
+  static create(payload: SorciEventCreatePayload, createId: createIdType = createIdDefault) {
     let identifier = payload.identifier ? payload.identifier : {};
     if (!payload.identifier) {
       identifier = SorciEvent.extractKeyValuePairContainingId(payload.data);
@@ -91,5 +93,34 @@ export class SorciEvent {
       identifier: this.identifier,
       timestamp: this.timestamp,
     };
+  }
+}
+
+/**
+ * This is a factory to create a SorciEvent class with a custom createId function
+ * 
+ * @example
+ * ```typescript
+ *  import { sorciEventFactory } from 'sorci';
+ *  import { v4 as createId } from 'uuid';
+ * 
+ *  const SorciEvent = sorciEventFactory(createId);
+ *   
+ *  const event = SorciEvent.create({
+ *    type: 'course-created', 
+ *    data: {
+ *      courseId: '6c811e1c-a441-4dc0-af89-b92f6e1784ed', 
+ *      name: 'Maths'
+ *    }
+ *  });
+ *  
+ *  console.log(event.id); // 98e4654d-ff79-4636-854b-7f51fcf54988 
+ * ```
+ */
+export function sorciEventFactory(createId: createIdType) {
+  return class SorciEventCustomized extends SorciEvent{
+    static create(payload: SorciEventCreatePayload) {
+      return SorciEvent.create(payload, createId);
+    }
   }
 }
